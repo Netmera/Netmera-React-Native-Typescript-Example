2648f94726528e64f3e43d16c572bf1b1ce50305
Nejdet Dalaslan <nejdet@initialcode.com.tr>
2023-12-26 21:01:05 +0300

    android and ios 0.68 -> 0.72.8 diff

--- ios/NetmeraTSExample/AppDelegate.h
+++ ios/NetmeraTSExample/AppDelegate.h
@@ -1,4 +1,4 @@
-#import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 #import <Netmera/Netmera.h>
 #import <NetmeraCore/NetmeraPushObject.h>
@@ -7,8 +7,6 @@
 #import "RNCConfig.h"
 
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate, NetmeraPushDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
+@interface AppDelegate : RCTAppDelegate
 
 @end
